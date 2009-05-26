cff43522a941a5e56b83e46b58f70339d74df03c
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-05-26 13:30:50 +0000

    license headers

--- trunk/src/java/com/drw/natenberg/Natenberg.java
+++ trunk/src/java/com/drw/natenberg/Natenberg.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.drw.natenberg;
 
 import java.io.IOException;
@@ -13,18 +31,18 @@ public class Natenberg {
 
 	private OtpNode node = null;
 	private OtpMbox mailbox = null;
-	
+
 	private final State start = new StateTransitionLogger(new StartState());
 	private final State receiving = new StateTransitionLogger(new ReceivingState());
 	private final State success = new StateTransitionLogger(new EndState(0));
 	private final State fail = new StateTransitionLogger(new EndState(1));
-	
+
 	private final View view = new View();
-	
+
 	private interface State{ // FSM
 		void transition();
 	}
-	
+
 	public static void main(String[] args){
 		new Natenberg().go();
 	}
@@ -32,7 +50,7 @@ public class Natenberg {
 	public void go(){
 		start.transition();
 	}
-	
+
 	private class StartState implements State{
 		public void transition() {
 			try {
@@ -44,7 +62,7 @@ public class Natenberg {
 			}
 		}
 	}
-	
+
 	private class ReceivingState implements State{
 		private Long sequence = 1l;
 		public void transition() {
@@ -66,7 +84,7 @@ public class Natenberg {
 			}
 		}
 	}
-	
+
 	private class EndState implements State{
 		private int returnValue;
 		EndState(int returnValue){
@@ -80,7 +98,7 @@ public class Natenberg {
 			System.exit(returnValue);
 		}
 	}
-	
+
 	private class StateTransitionLogger implements State{
 		private final State wrapped;
 		public StateTransitionLogger(State wrapped) {
@@ -93,5 +111,5 @@ public class Natenberg {
 			System.out.println("Exit " + name);
 		}
 	}
-	
+
 }

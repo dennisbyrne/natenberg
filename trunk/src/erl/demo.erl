1e6df216511c39aab16edc48d7b3eca4629bc640
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-06 16:41:57 +0000

    moved all demo code into a module called demo

---
+++ trunk/src/erl/demo.erl
@@ -0,0 +1,50 @@
+-module(demo).
+-export([pages/0, page15/0, page16/0, page17/0, page18/0, page19/0, page20/0, 
+		 page21/0, page22/0, page23/0, page24/0, page26/0, page29/0, page139/0]).
+-include_lib("position.hrl").
+
+pages() ->
+	timer:start(),
+	Functions = [page15, page16, page17, page18, page19, page20, 
+				 page21, page22, page23, page24, page26, page29, 
+				 page139],
+	[ timer:apply_after(Seq * 1000, demo, lists:nth(Seq, Functions), []) || Seq <- lists:seq(1, length(Functions)) ].
+
+page15() ->
+	chapter2:draw(?LONG_UNDERLYING).
+
+page16() ->
+	chapter2:draw(?LONG_CALL).
+
+page17() ->
+	chapter2:draw(?SHORT_CALL).
+
+page18() ->
+	chapter2:draw(?LONG_PUT).
+
+page19() ->
+	chapter2:draw(?SHORT_PUT).
+
+page20() ->
+	chapter2:draw(?LONG_STRADDLE).	
+
+page21() ->
+	chapter2:draw(?SHORT_STRADDLE).
+
+page22() ->
+	chapter2:draw(?SHORT_STRANGLE).
+
+page23() ->
+	chapter2:draw(?PAGE_23).
+
+page24() ->
+	chapter2:draw(?PAGE_24).
+
+page26() ->
+	chapter2:draw(?CALL_RATIO_VERTICAL_SPREAD).
+
+page29() ->
+	chapter2:draw(?PAGE_29).
+
+page139() ->
+	chapter2:draw(?PUT_BACKSPREAD).

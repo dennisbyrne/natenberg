a53ce3b044558296b2cd03d9db782f5977f64d02
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-06 22:12:46 +0000

    added two butterflies

--- trunk/src/erl/demo.erl
+++ trunk/src/erl/demo.erl
@@ -1,14 +1,14 @@
 -module(demo).
 -export([pages/0, page15/0, page16/0, page17/0, page18/0, page19/0, page20/0, 
 		 page21/0, page22/0, page23/0, page24/0, page26/0, page29/0, page138/0,
-		 page139/0, page140/0, page143/0]).
+		 page139/0, page140/0, page143/0, page146/0, page147/0]).
 -include_lib("position.hrl").
 
 pages() ->
 	timer:start(),
 	Functions = [page15, page16, page17, page18, page19, page20, 
 				 page21, page22, page23, page24, page26, page29, 
-				 page138, page139, page140, page143],
+				 page138, page139, page140, page143, page146, page147],
 	[ timer:apply_after(Seq * 1000, demo, lists:nth(Seq, Functions), []) || Seq <- lists:seq(1, length(Functions)) ].
 
 page15() ->
@@ -59,3 +59,8 @@ page140() ->
 page143() ->
 	chapter2:draw(?LONG_STRANGLE).
 	
+page146() ->
+	chapter2:draw(?LONG_BUTTERFLY).
+
+page147() ->
+	chapter2:draw(?SHORT_BUTTERFLY).

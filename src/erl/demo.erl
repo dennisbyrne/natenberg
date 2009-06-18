fe14931600523580c1915a2fbca8b4690d63b9e2
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-18 03:05:54 +0000

    short condors

--- src/erl/demo.erl
+++ src/erl/demo.erl
@@ -3,7 +3,7 @@
 		 page21/0, page22/0, page23/0, page24/0, page26/0, page29/0, page138/0,
 		 page139/0, page140/0, page143/0, page146/0, page147/0, page158a/0, 
 		 page158b/0, page158c/0, page158d/0, page159a/0, page159b/0, page159c/0,
-		 page159d/0]).
+		 page159d/0, page159e/0, page159f/0]).
 -include_lib("position.hrl").
 
 pages() ->
@@ -12,7 +12,8 @@ pages() ->
 				 page21, page22, page23, page24, page26, page29, 
 				 page138, page139, page140, page143, page146, 
 				 page147, page158a, page158b, page158c, page158d,
-				 page159a, page159b, page159c, page159d],
+				 page159a, page159b, page159c, page159d, page159e,
+				 page159f],
 	[ timer:apply_after(Seq * 1000, demo, lists:nth(Seq, Functions), []) || Seq <- lists:seq(1, length(Functions)) ].
 
 page15() ->
@@ -92,3 +93,9 @@ page159c() ->
 
 page159d() ->
 	chapter2:draw(?LONG_PUT_CONDOR).
+
+page159e() ->
+	chapter2:draw(?SHORT_CALL_CONDOR).
+
+page159f() ->
+	chapter2:draw(?SHORT_PUT_CONDOR).

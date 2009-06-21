0004816de99e0b924a50ae2fb5726beadf933ece
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-21 16:54:12 +0000

    shorted descriptions, added function to show many short calls

--- src/erl/struct.hrl
+++ src/erl/struct.hrl
@@ -18,13 +18,13 @@
 -define(UNDERLYING, #side{underlyings = [#underlying{px = 99.0}]}).
 
 -define(LONG_UNDERLYING, 
-		#position{description = "Long Underlying, Page 15", long = ?UNDERLYING}).
+		#position{description = "Long Underlying on Pg 15", long = ?UNDERLYING}).
 
 -define(SHORT_UNDERLYING, 
-		#position{description = "Short Underlying, Page 15", short = ?UNDERLYING}).
+		#position{description = "Short Underlying on Pg 15", short = ?UNDERLYING}).
 
 -define(LONG_CALL, 
-		#position{description = "Long Call, page 16",
+		#position{description = "Long Call on Pg 16",
 				  long = #side{calls = [#option{px = 2.7, strike = 100.0}]}}).
 
 -define(SHORT_CALL, 
@@ -55,12 +55,12 @@
 				  				puts = [#option{px = 1.55, strike = 95.0}]}}).
 
 -define(CALL_BULL_SPREAD,
-		#position{description = "Call Bull Spread, Page 23",
+		#position{description = "Call Bull Spread on Pg 23",
 				  long = #side{calls = [#option{px = 9.35, strike = 90.0}]}, 
 				  short = #side{calls = [#option{px = 2.7, strike = 100.0}]}}).
 
 -define(PUT_BEAR_SPREAD,
-		#position{description = "Put Bear Spread, Page 24",
+		#position{description = "Put Bear Spread on Pg 24",
 				  long = #side{puts = [#option{px = 7.1, strike = 105.0}]}, 
 				  short = #side{puts = [#option{px = 3.7, strike = 100.0}]}}).
 
@@ -91,88 +91,88 @@
 				  short = #side{puts = lists:duplicate(30, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0})}}).
 
 -define(PUT_RATIO_VERTICAL_SPREAD,
-		#position{description = "Put Ratio Verical Spread, Page 140",
+		#position{description = "Put Ratio Verical Spread on Pg 140",
 				  long = #side{puts = lists:duplicate(20, (?MARCH_OPTION)#option{px = 0.85, strike = 95.0})},
 				  short = #side{puts = lists:duplicate(60, (?MARCH_OPTION)#option{px = 0.17, strike = 90.0})}}).
 
 -define(LONG_STRANGLE,
-		#position{description = "Long Strangle, Page 143",
+		#position{description = "Long Strangle on Pg 143",
 				  long = #side{calls = lists:duplicate(20, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0}),
 							   puts = lists:duplicate(20, (?MARCH_OPTION)#option{px = 5.82, strike = 95.0})}}).
 
 -define(LONG_BUTTERFLY,
-		#position{description = "Long Butterfly, Page 146",
+		#position{description = "Long Butterfly on Pg 146",
 				  long = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 5.82, strike = 95.0}) ++
 							  		   lists:duplicate(10, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0})},
 				  short = #side{calls = lists:duplicate(20, (?MARCH_OPTION)#option{px = 2.69, strike = 100.0})}}).
 
 -define(SHORT_BUTTERFLY,
-		#position{description = "Short Butterfly, Page 146",
+		#position{description = "Short Butterfly on Pg 146",
 				  long = #side{puts = lists:duplicate(50, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0})},
 				  short = #side{puts = lists:duplicate(25, (?JUNE_OPTION)#option{px = 2.55, strike = 95.0}) ++
 									   lists:duplicate(25, (?JUNE_OPTION)#option{px = 7.66, strike = 105.0})}}).
 
 -define(LONG_CALL_XMAS_TREE,
-		#position{description = "Long Call Christmas Tree, Page 158",
+		#position{description = "Long Call Christmas Tree on Pg 158",
 				  long = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 5.82, strike = 95.0})},
 				  short = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 2.69, strike = 100.0}) ++
 										lists:duplicate(10, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0})}}).
 
 -define(LONG_PUT_XMAS_TREE,
-		#position{description = "Long Put Christmas Tree, Page 158",
+		#position{description = "Long Put Christmas Tree on Pg 158",
 				  long = #side{puts = lists:duplicate(25, (?JUNE_OPTION)#option{px = 11.3, strike = 110.0})},
 				  short = #side{puts = lists:duplicate(25, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0}) ++
 									   lists:duplicate(25, (?JUNE_OPTION)#option{px = 2.55, strike = 95.0})}}).
 
 -define(SHORT_CALL_XMAS_TREE,
-		#position{description = "Short Call Christmas Tree, Page 158",
+		#position{description = "Short Call Christmas Tree on Pg 158",
 				  long = #side{calls = lists:duplicate(5, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0}) ++
 									   lists:duplicate(5, (?JUNE_OPTION)#option{px = 2.77, strike = 105.0})},
 				  short = #side{calls = lists:duplicate(5, (?JUNE_OPTION)#option{px = 10.97, strike = 90.0})}}).
 
 -define(SHORT_PUT_XMAS_TREE,
-		#position{description = "Short Put Christmas Tree, Page 158",
+		#position{description = "Short Put Christmas Tree on Pg 158",
 				  long = #side{puts = lists:duplicate(50, (?MARCH_OPTION)#option{px = 5.92, strike = 105.0}) ++
 									  lists:duplicate(50, (?MARCH_OPTION)#option{px = 0.85, strike = 95.0})},
 				  short = #side{puts = lists:duplicate(50, (?MARCH_OPTION)#option{px = 10.16, strike = 110.0})}}).
 
 -define(LONG_IRON_BUTTERFLY, 
-		#position{description = "Long Iron Butterfly, Page 159",
+		#position{description = "Long Iron Butterfly on Pg 159",
 				  long = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 2.69, strike = 100.0}),
 							   puts = lists:duplicate(10, (?MARCH_OPTION)#option{px = 2.68, strike = 100.0})},
 				  short = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0}),
 								puts = lists:duplicate(10, (?MARCH_OPTION)#option{px = 0.85, strike = 95.0})}}).
 
 -define(SHORT_IRON_BUTTERFLY,
-		#position{description = "Short Iron Butterfly, Page 159",
+		#position{description = "Short Iron Butterfly on Pg 159",
 				  long = #side{calls = lists:duplicate(15, (?JUNE_OPTION)#option{px = 2.77, strike = 105.0}),
 							   puts = lists:duplicate(15, (?JUNE_OPTION)#option{px = 2.55, strike = 95.0})},
 				  short = #side{calls = lists:duplicate(15, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0}),
 							    puts = lists:duplicate(15, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0})}}).
 
 -define(LONG_CALL_CONDOR,
-   		#position{description = "Long Condor, Page 159",
+   		#position{description = "Long Condor on Pg 159",
 				  long = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 10.1, strike = 90.0}) ++
 									   lists:duplicate(10, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0})},
 				  short = #side{calls = lists:duplicate(10, (?MARCH_OPTION)#option{px = 5.82, strike = 95.0}) ++
 										lists:duplicate(10, (?MARCH_OPTION)#option{px = 2.69, strike = 100.0})}}).
 
 -define(LONG_PUT_CONDOR,
-		#position{description = "Long Put Condor, Page 159",
+		#position{description = "Long Put Condor on Pg 159",
 				  long = #side{puts = lists:duplicate(25, (?JUNE_OPTION)#option{px = 2.55, strike = 95.0}) ++
 									  lists:duplicate(25, (?JUNE_OPTION)#option{px = 11.33, strike = 110.0})},
 				  short = #side{puts = lists:duplicate(25, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0}) ++
 									   lists:duplicate(25, (?JUNE_OPTION)#option{px = 7.66, strike = 105.0})}}).
 
 -define(SHORT_CALL_CONDOR,
-		#position{description = "Short Call Condor, Page 159",
+		#position{description = "Short Call Condor on Pg 159",
 				  long = #side{calls = lists:duplicate(50, (?MARCH_OPTION)#option{px = 2.69, strike = 100.0}) ++
 									   lists:duplicate(50, (?MARCH_OPTION)#option{px = 0.95, strike = 105.0})},
 				  short = #side{calls = lists:duplicate(50, (?MARCH_OPTION)#option{px = 5.82, strike = 95.0}) ++
 										lists:duplicate(50, (?MARCH_OPTION)#option{px = 0.26, strike = 110.0})}}).
 
 -define(SHORT_PUT_CONDOR,
-		#position{description = "Short Put Condor, Page 159",
+		#position{description = "Short Put Condor on Pg 159",
 				  long = #side{puts = lists:duplicate(5, (?JUNE_OPTION)#option{px = 2.55, strike = 95.0}) ++
 									  lists:duplicate(5, (?JUNE_OPTION)#option{px = 4.71, strike = 100.0})},
 				  short = #side{puts = lists:duplicate(5, (?JUNE_OPTION)#option{px = 1.18, strike = 90.0}) ++

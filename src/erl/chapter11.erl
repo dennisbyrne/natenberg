c756b6b96947e6c933b7cb7d5aca14e37d87ea3d
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-21 21:03:48 +0000

    fixing logic error

--- src/erl/chapter11.erl
+++ src/erl/chapter11.erl
@@ -12,17 +12,20 @@ conversion(Position, ToOption) ->
 	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Long#side.underlyings],
 	CallPxs = [ {CallPx, UnderlyingPx} || {{CallPx, _}, UnderlyingPx} <- Pxs],
 	PutPxs = [ {PutPx, UnderlyingPx} || {{_, PutPx}, UnderlyingPx} <- Pxs],
-	#position{description = "Synthetic of " ++ Desc,
-			  long = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]},
-			  short = #side{puts = [#option{px = Px, strike = Strike} || {Px, Strike} <- PutPxs ]}}.
+	#position{description = "Conversion of " ++ Desc,
+			  long = #side{underlyings = [ #underlying{px = Px} || {_, Px} <- Pxs],
+						   puts = [#option{px = Px, strike = Strike} || {Px, Strike} <- PutPxs ]},
+			  short = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]}}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Unit Tests
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-conversion_test() ->
-	ToOption = dict:from_list([{?UNDERLYING, {3.0, 5.0}}]),
-	Synthetic = #position{description = "Synthetic of Long Underlying on Pg 15", 
-						  long = #side{calls = [#option{px = 3.0, strike = 99.0}]},
-						  short = #side{puts = [#option{px = 5.0, strike = 99.0}]}},
-	?assertEqual(Synthetic, conversion(?LONG_UNDERLYING, ToOption)).
+-define(T0_OPTION, dict:from_list([{?UNDERLYING, {3.0, 5.0}}])).
+
+conversion_long_test() ->
+	Synthetic = #position{description = "Conversion of Long Underlying on Pg 15", 
+						  long = #side{underlyings = [?UNDERLYING],
+									   puts = [#option{px = 5.0, strike = 99.0}]},
+						  short = #side{calls = [#option{px = 3.0, strike = 99.0}]}},
+	?assertEqual(Synthetic, conversion(?LONG_UNDERLYING, ?T0_OPTION)).

992a1925b9ee86c706805e9097a0a8062b208b5e
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-21 21:20:56 +0000

    added merge/2 and merge/3

--- trunk/src/erl/chapter11.erl
+++ trunk/src/erl/chapter11.erl
@@ -8,14 +8,28 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 conversion(Position, ToOption) ->
-	#position{description = Desc, long = Long, short = _} = Position,
-	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Long#side.underlyings],
+	#position{description = Desc, long = Long, short = Short} = Position,
+	LongPosition = conversion2(Long, ToOption),
+	ShortPosition = conversion2(Short, ToOption),
+	merge(LongPosition, ShortPosition, "Conversion of " ++ Desc).
+	
+conversion2(Side, ToOption) ->
+	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Side#side.underlyings],
 	CallPxs = [ {CallPx, UnderlyingPx} || {{CallPx, _}, UnderlyingPx} <- Pxs],
 	PutPxs = [ {PutPx, UnderlyingPx} || {{_, PutPx}, UnderlyingPx} <- Pxs],
-	#position{description = "Conversion of " ++ Desc,
-			  long = #side{underlyings = [ #underlying{px = Px} || {_, Px} <- Pxs],
+	#position{long = #side{underlyings = [ #underlying{px = Px} || {_, Px} <- Pxs],
 						   puts = [#option{px = Px, strike = Strike} || {Px, Strike} <- PutPxs ]},
 			  short = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]}}.
+
+merge(To, From, Description) ->
+	#position{description = Description, 
+			  long = merge(To#position.long, From#position.long), 
+			  short = merge(To#position.short, From#position.short)}.
+
+merge(To, From) ->
+	#side{calls = To#side.calls ++ From#side.calls,
+		  puts = To#side.puts ++ From#side.puts,
+		  underlyings = To#side.underlyings ++ From#side.underlyings}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Unit Tests

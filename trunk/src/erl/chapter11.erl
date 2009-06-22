f0fefe0c1fd45a118b2527ce4feb8561e13f70fa
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-22 01:31:09 +0000

    no functional changes

--- trunk/src/erl/chapter11.erl
+++ trunk/src/erl/chapter11.erl
@@ -7,32 +7,23 @@
 % Implementation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-conversion(#side{underlyings = Underlyings}, ToOption) ->
-	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Underlyings],
-	CallPxs = [ {CallPx, UnderlyingPx} || {{CallPx, _}, UnderlyingPx} <- Pxs],
-	PutPxs = [ {PutPx, UnderlyingPx} || {{_, PutPx}, UnderlyingPx} <- Pxs],
-	#position{long = #side{underlyings = [ #underlying{px = Px} || {_, Px} <- Pxs],
-						   puts = [#option{px = Px, strike = Strike} || {Px, Strike} <- PutPxs ]},
-			  short = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]}};
 conversion(Position, ToOption) ->
-	LongPosition = conversion(Position#position.long, ToOption),
-	ShortPosition = invert(conversion(Position#position.short, ToOption)),
-	merge(LongPosition, ShortPosition, "Conversion of " ++ Position#position.description).
+	LongUnderlyings = (Position#position.long)#side.underlyings,
+	ShortUnderlyings = (Position#position.short)#side.underlyings,
+	{ShortCalls, LongPuts} = convert(LongUnderlyings, ToOption),
+	{LongCalls, ShortPuts} = convert(ShortUnderlyings, ToOption),
+	#position{description = "Conversion of " ++ Position#position.description,
+			  long = #side{underlyings = LongUnderlyings,
+						   calls = LongCalls,
+						   puts = LongPuts},
+			  short = #side{underlyings = ShortUnderlyings,
+						    calls = ShortCalls,
+						    puts = ShortPuts}}.
 
-invert(Position) ->
-	#position{description = Position#position.description,
-			  long = Position#position.short,
-			  short = Position#position.long}.
-
-merge(To, From, Description) ->
-	#position{description = Description,
-			  long = merge(To#position.long, From#position.long),
-			  short = merge(To#position.short, From#position.short)}.
-
-merge(To, From) ->
-	#side{calls = To#side.calls ++ From#side.calls,
-		  puts = To#side.puts ++ From#side.puts,
-		  underlyings = To#side.underlyings ++ From#side.underlyings}.
+convert(Underlyings, ToOption) ->
+	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Underlyings],
+	{[ #option{px = Px, strike = Strike} || {{Px, _}, Strike} <- Pxs],
+	 [ #option{px = Px, strike = Strike} || {{_, Px}, Strike} <- Pxs]}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Unit Tests

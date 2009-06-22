36511ea16ef6792c135834d9567fe524b6b89d0b
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-22 02:17:14 +0000

    no functional changes

--- src/erl/chapter11.erl
+++ src/erl/chapter11.erl
@@ -8,23 +8,31 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 conversion(Position, ToOption) ->
-	LongUnderlyings = (Position#position.long)#side.underlyings,
-	{ShortCalls, LongPuts} = synthesize(LongUnderlyings, ToOption),
-	#position{description = "Converted " ++ Position#position.description,
-			  long = #side{underlyings = LongUnderlyings, puts = LongPuts},
-			  short = #side{calls = ShortCalls}}.
+	Underlyings = (Position#position.long)#side.underlyings,
+	{Puts, Calls} = put_call_parity(Underlyings, ToOption),
+	Synthetic = #position{long = #side{puts = Puts}, short = #side{calls = Calls}},
+	merge(Synthetic, Position, "Converted " ++ Position#position.description).
 
 reversal(Position, ToOption) ->
-	ShortUnderlyings = (Position#position.short)#side.underlyings,
-	{LongCalls, ShortPuts} = synthesize(ShortUnderlyings, ToOption),
-	#position{description = "Reversed " ++ Position#position.description,
-			  long = #side{calls = LongCalls},
-			  short = #side{underlyings = ShortUnderlyings, puts = ShortPuts}}.	
+	Underlyings = (Position#position.short)#side.underlyings,
+	{Puts, Calls} = put_call_parity(Underlyings, ToOption),
+	Synthetic = #position{long = #side{calls = Calls}, short = #side{puts = Puts}},
+	merge(Synthetic, Position, "Reversed " ++ Position#position.description).
 
-synthesize(Underlyings, ToOption) ->
+put_call_parity(Underlyings, ToOption) ->
 	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Underlyings],
-	{[ #option{px = Px, strike = Strike} || {{Px, _}, Strike} <- Pxs],
-	 [ #option{px = Px, strike = Strike} || {{_, Px}, Strike} <- Pxs]}.
+	{[ #option{px = Px, strike = Strike} || {{_, Px}, Strike} <- Pxs],
+	 [ #option{px = Px, strike = Strike} || {{Px, _}, Strike} <- Pxs]}.
+
+merge(To, From, Description) ->
+	#position{description = Description,
+			  long = merge(To#position.long, From#position.long), 
+			  short = merge(To#position.short, From#position.short)}.
+
+merge(To, From) ->
+	#side{calls = To#side.calls ++ From#side.calls,
+		  puts = To#side.puts ++ From#side.puts,
+		  underlyings = To#side.underlyings ++ From#side.underlyings}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Unit Tests
@@ -42,14 +50,14 @@ combined_conversion_test() ->
 	Pnl = chapter1:pnl(10.0, Combined),
 	Pnl = chapter1:pnl(34.2, Combined).
 
-reversal_short_test() ->
+reversal_test() ->
 	Combined = #position{description = "Reversed Short Underlying on Pg 15", 
 						 long = #side{calls = [#option{px = 3.0, strike = 99.0}]},
 						 short = #side{underlyings = [?UNDERLYING],
 									   puts = [#option{px = 5.0, strike = 99.0}]}},
 	?assertEqual(Combined, reversal(?SHORT_UNDERLYING, ?TO_OPTION)).
 
-conversion_long_test() ->
+conversion_test() ->
 	Combined = #position{description = "Converted Long Underlying on Pg 15", 
 						 long = #side{underlyings = [?UNDERLYING],
 									  puts = [#option{px = 5.0, strike = 99.0}]},

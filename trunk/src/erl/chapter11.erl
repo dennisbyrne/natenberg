ea8b83425ec9f8658a25a78dcce19fff170dc8fd
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-06-22 01:17:24 +0000

    now converting the short side

--- trunk/src/erl/chapter11.erl
+++ trunk/src/erl/chapter11.erl
@@ -7,23 +7,26 @@
 % Implementation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-conversion(Position, ToOption) ->
-	#position{description = Desc, long = Long, short = Short} = Position,
-	LongPosition = conversion2(Long, ToOption),
-	ShortPosition = conversion2(Short, ToOption),
-	merge(LongPosition, ShortPosition, "Conversion of " ++ Desc).
-	
-conversion2(Side, ToOption) ->
-	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Side#side.underlyings],
+conversion(#side{underlyings = Underlyings}, ToOption) ->
+	Pxs = [ {dict:fetch(U, ToOption), U#underlying.px} || U <- Underlyings],
 	CallPxs = [ {CallPx, UnderlyingPx} || {{CallPx, _}, UnderlyingPx} <- Pxs],
 	PutPxs = [ {PutPx, UnderlyingPx} || {{_, PutPx}, UnderlyingPx} <- Pxs],
 	#position{long = #side{underlyings = [ #underlying{px = Px} || {_, Px} <- Pxs],
 						   puts = [#option{px = Px, strike = Strike} || {Px, Strike} <- PutPxs ]},
-			  short = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]}}.
+			  short = #side{calls = [#option{px = Px, strike = Strike} || {Px, Strike} <- CallPxs ]}};
+conversion(Position, ToOption) ->
+	LongPosition = conversion(Position#position.long, ToOption),
+	ShortPosition = invert(conversion(Position#position.short, ToOption)),
+	merge(LongPosition, ShortPosition, "Conversion of " ++ Position#position.description).
+
+invert(Position) ->
+	#position{description = Position#position.description,
+			  long = Position#position.short,
+			  short = Position#position.long}.
 
 merge(To, From, Description) ->
-	#position{description = Description, 
-			  long = merge(To#position.long, From#position.long), 
+	#position{description = Description,
+			  long = merge(To#position.long, From#position.long),
 			  short = merge(To#position.short, From#position.short)}.
 
 merge(To, From) ->
@@ -37,9 +40,16 @@ merge(To, From) ->
 
 -define(T0_OPTION, dict:from_list([{?UNDERLYING, {3.0, 5.0}}])).
 
+conversion_short_test() ->
+	Combined = #position{description = "Conversion of Short Underlying on Pg 15", 
+						 long = #side{calls = [#option{px = 3.0, strike = 99.0}]},
+						 short = #side{underlyings = [?UNDERLYING],
+									   puts = [#option{px = 5.0, strike = 99.0}]}},
+	?assertEqual(Combined, conversion(?SHORT_UNDERLYING, ?T0_OPTION)).
+
 conversion_long_test() ->
-	Synthetic = #position{description = "Conversion of Long Underlying on Pg 15", 
-						  long = #side{underlyings = [?UNDERLYING],
-									   puts = [#option{px = 5.0, strike = 99.0}]},
-						  short = #side{calls = [#option{px = 3.0, strike = 99.0}]}},
-	?assertEqual(Synthetic, conversion(?LONG_UNDERLYING, ?T0_OPTION)).
+	Combined = #position{description = "Conversion of Long Underlying on Pg 15", 
+						 long = #side{underlyings = [?UNDERLYING],
+									  puts = [#option{px = 5.0, strike = 99.0}]},
+						 short = #side{calls = [#option{px = 3.0, strike = 99.0}]}},
+	?assertEqual(Combined, conversion(?LONG_UNDERLYING, ?T0_OPTION)).

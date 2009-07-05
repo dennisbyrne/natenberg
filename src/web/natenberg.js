4c8e3820746b105ad555423cf53bf3f0eb62302f
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-07-05 22:28:22 +0000

    now showing the first option on 1st render

--- src/web/natenberg.js
+++ src/web/natenberg.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $("select").change(function(){
         $("select option:selected").each(function(){
-            $.get($(this)[0].value + ".json", function(data){
-                $("#graph")[0].draw(data);
-                $("#table_heading")[0].innerHTML = eval("(" + data + ")").description;
-            });
+            show($(this)[0].value);
         });
     });
+    show($("option:first")[0].value);
 });
+
+show = function(functionName){
+    $.get(functionName + ".json", function(data){
+        $("#graph")[0].draw(data);
+        $("#table_heading")[0].innerHTML = eval("(" + data + ")").description;
+    });
+};

979d0a44e7a70584ec09288b48c44f787d046b20
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-07-05 22:10:05 +0000

    collapsed a method

--- trunk/src/web/natenberg.js
+++ trunk/src/web/natenberg.js
@@ -1,17 +1,10 @@
- $(document).ready(function(){
+$(document).ready(function(){
     $("select").change(function(){
         $("select option:selected").each(function(){
-            callback($(this)[0].value)();
+            $.get($(this)[0].value + ".json", function(data){
+                $("#graph")[0].draw(data);
+                $("#table_heading")[0].innerHTML = eval("(" + data + ")").description;
+            });
         });
     });
- });
- 
- function callback(functionName){
-    return function(){
-        $.get(functionName + ".json", function(data){
-            $("#graph")[0].draw(data);
-            $("#table_heading")[0].innerHTML = eval("(" + data + ")").description;
-        });
-    };
- }
-
+});

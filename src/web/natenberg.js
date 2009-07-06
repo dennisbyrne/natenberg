39146e997044e994f9d33ed2e562c6afba96cf7c
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-07-06 00:31:44 +0000

    now showing wire protocal at bottom

--- src/web/natenberg.js
+++ src/web/natenberg.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
 show = function(functionName){
     $.get(functionName + ".json", function(data){
         $("#graph")[0].draw(data);
+        $("#wire")[0].innerHTML = data;
         $("#table_heading")[0].innerHTML = eval("(" + data + ")").description;
     });
 };

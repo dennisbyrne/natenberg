5f5bcfc6768fe7fbbdc800fa28c6090051dd2217
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-12-26 22:03:58 +0000

    x86 samples

--- memory/src/protocol/command.sh
+++ memory/src/protocol/command.sh
@@ -1,7 +1,8 @@
-/usr/lib/jvm/java-6-openjdk/bin/javac WriterReader.java 
-/usr/lib/jvm/java-6-openjdk/jre/bin/java -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,WriterReader.write WriterReader > write.txt
-/usr/lib/jvm/java-6-openjdk/jre/bin/java -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,WriterReader.read WriterReader > read.txt
+# set JAVAC=/usr/java/jdk1.6.0_17/bin/javac
+# set JAVA=/usr/java/jdk1.6.0_17/bin/java
+set JAVAC=/usr/lib/jvm/java-6-openjdk/bin/javac
+set JAVA=/usr/lib/jvm/java-6-openjdk/jre/bin/java
 
-/usr/java/jdk1.6.0_17/bin/javac com/drw/membar/*.java
-/usr/java/jdk1.6.0_17/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/FirstThread.protocol com.drw.membar.Dekker > first.txt
-/usr/java/jdk1.6.0_17/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/SecondThread.protocol com.drw.membar.Dekker > second.txt
+/usr/lib/jvm/java-6-openjdk/bin/javac com/drw/membar/*.java
+/usr/lib/jvm/java-6-openjdk/jre/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/FirstThread.protocol com.drw.membar.Dekker > first.txt
+/usr/lib/jvm/java-6-openjdk/jre/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/SecondThread.protocol com.drw.membar.Dekker > second.txt

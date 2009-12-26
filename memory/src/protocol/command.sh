d1079d2739af8899a7d08c64eb5f3dcf0f53a8da
dennis.byrne <dennis.byrne@14c33404-4647-11de-aeda-3f4d37dfb5fa>
2009-12-26 21:53:37 +0000

    cleaned up script, removed WriterReader.java

--- memory/src/protocol/command.sh
+++ memory/src/protocol/command.sh
@@ -2,8 +2,6 @@
 /usr/lib/jvm/java-6-openjdk/jre/bin/java -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,WriterReader.write WriterReader > write.txt
 /usr/lib/jvm/java-6-openjdk/jre/bin/java -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,WriterReader.read WriterReader > read.txt
 
-
-
 /usr/java/jdk1.6.0_17/bin/javac com/drw/membar/*.java
 /usr/java/jdk1.6.0_17/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/FirstThread.protocol com.drw.membar.Dekker > first.txt
 /usr/java/jdk1.6.0_17/bin/java -cp . -server -XX:+UnlockDiagnosticVMOptions -XX:PrintAssemblyOptions=hsdis-print-bytes -XX:CompileCommand=print,com/drw/membar/SecondThread.protocol com.drw.membar.Dekker > second.txt
